7133eadd394b22e5d5f3f211d5efa186dac2d9e7
swaraj bachu <rajswaraj.r@gmail.com>
2023-10-03 18:55:39 +0700

    some changes to voting

--- src/compnents/vote.js
+++ src/compnents/vote.js
@@ -1,87 +1,194 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+const maxTotalVotes = 60;
+const maxVotesPerCandidate = 20;
 
 const candidates = [
   {
     first: "Alex",
     last: "Popp",
-    votes: 0
+    votes: 0,
   },
   {
     first: "George",
     last: "Washington",
-    votes: 0
+    votes: 0,
   },
   {
     first: "Abraham",
     last: "Lincoln",
-    votes: 0
+    votes: 0,
   },
   {
     first: "Santa",
     last: "Claus",
-    votes: 0
-  }, 
+    votes: 0,
+  },
   {
     first: "Johnny",
     last: "Cash",
-    votes: 0
-  }
+    votes: 0,
+  },
 ];
 
 const VotingComponent = () => {
   const [raceOver, setRaceOver] = useState(false);
+  const [totalVotes, setTotalVotes] = useState(0);
 
-  const sendVote = (index) => {
-    if (raceOver) {
-      return;
-    }
+  const sendVote = (index, voteType) => {
     const updatedCandidates = [...candidates];
-    updatedCandidates[index].votes++;
-    if (updatedCandidates[index].votes === 5) {
-      const winnerName = `${updatedCandidates[index].first} ${updatedCandidates[index].last}`;
-      wonRace(winnerName);
-    } else {
-      setCandidates(updatedCandidates);
-      winningRace(updatedCandidates);
+    if (
+      voteType === "up" &&
+      totalVotes < maxTotalVotes &&
+      updatedCandidates[index].votes < maxVotesPerCandidate
+    ) {
+      updatedCandidates[index].votes = updatedCandidates[index].votes + 2;
+      setTotalVotes(totalVotes + 2);
+    } else if (voteType === "down" && updatedCandidates[index].votes > 0) {
+      updatedCandidates[index].votes = updatedCandidates[index].votes - 2;
+      setTotalVotes(totalVotes - 2);
     }
+
+    setCandidates(updatedCandidates);
   };
 
   const [stateCandidates, setCandidates] = useState(candidates);
 
-  const winningRace = (candidates) => {
-    const sortedCandidates = [...candidates].sort((a, b) => b.votes - a.votes);
-    const firstName = sortedCandidates[0].first + ' ' + sortedCandidates[0].last;
-    const secondName = sortedCandidates[1].first + ' ' + sortedCandidates[1].last;
-
-    if (sortedCandidates[0].votes === sortedCandidates[1].votes) {
-      return `The race is tied between ${firstName} and ${secondName}!`;
-    }
-
-    return `${firstName} is winning the race!`;
-  };
-
-  const wonRace = (winnerName) => {
+  const endRace = (candidates) => {
     setRaceOver(true);
     // Additional logic for the race being over
   };
 
   return (
-    <div className={`container ${raceOver ? 'votingDone' : ''}`}>
-      <div className="winning">{raceOver ? 'Race Over!' : 'Vote for a Candidate!'}</div>
-      <div className="votingContainer">
-        {stateCandidates.map((candidate, index) => (
-          <div className={`singleCandidate vote${candidate.votes}`} key={index}>
-            <div className="name">{`${candidate.first} ${candidate.last}`}</div>
-            <div className="votes">{candidate.votes}</div>
-            {!raceOver && (
-              <button onClick={() => {sendVote(index)
-            console.log("vote sent")
-            }}>Vote</button>
-            )}
-          </div>
-        ))}
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        color: "black",
+        padding: "20px",
+        borderRadius: "10px",
+        marginTop: "20px",
+        fontWeight: "bold",
+      }}
+    >
+      <h4
+        style={{
+          fontFamily: "Arial, sans-serif",
+          margin: "10px",
+          borderRadius: "10px",
+          marginTop: "20px",
+          background: "black",
+          color: "white",
+          padding: "5px 20px",
+          lineBreak: "auto",
+        }}
+      >
+        {maxTotalVotes === totalVotes
+          ? "Out of Funds"
+          : `Total Funds Available: $${maxTotalVotes - totalVotes}`}
+      </h4>
+
+      <div>
+        {stateCandidates.map((candidate, index) => {
+          const percentage =
+            (candidate.votes / maxVotesPerCandidate) * 100 + "%";
+
+          return (
+            <div
+              key={index}
+              style={{
+                background: `linear-gradient(to right, #a2d2ff ${percentage}, transparent ${percentage})`,
+                display: "flex",
+                justifyContent: "space-between",
+                border: "2px solid black",
+                borderRadius: "5px",
+                margin: "10px",
+              }}
+            >
+              {candidate.votes > 0 ? (
+                <button
+                  style={{
+                    background: "#ef233c",
+                    border: "none",
+                    color: "white",
+                    borderRadius: "5px",
+                    margin: "10px",
+                  }}
+                  onClick={() => sendVote(index, "down")}
+                >
+                  Down Vote
+                </button>
+              ) : null}
+              <div
+                style={{
+                  padding: "10px",
+                  display: "flex",
+                  gap: "10px",
+                  alignItems: "center",
+                  color: "black",
+                }}
+              >
+                <p>{`${candidate.first} ${candidate.last}`}</p>
+                <p>${candidate.votes}</p>
+              </div>
+              {candidate.votes < maxVotesPerCandidate ? (
+                <button
+                  style={{
+                    background: "#38b000",
+                    border: "none",
+                    color: "white",
+                    padding: "5px 20px",
+                    lineBreak: "auto",
+                    borderRadius: "5px",
+                    margin: "10px",
+                  }}
+                  onClick={() => sendVote(index, "up")}
+                >
+                  Vote Up
+                </button>
+              ) : (
+                <div
+                  style={{
+                    background: "black",
+                    border: "none",
+                    color: "white",
+                    padding: "5px 20px",
+                    margin: "10px",
+                    borderRadius: "5px",
+                  }}
+                ></div>
+              )}
+            </div>
+          );
+        })}
       </div>
-      {raceOver && <div className="winning">{winningRace(stateCandidates)}</div>}
+      {maxTotalVotes === totalVotes && (
+        <div
+          style={{
+            background: "black",
+            border: "none",
+            color: "white",
+            padding: "5px 20px",
+            textAlign: "center",
+            borderRadius: "5px",
+            margin: "10px",
+          }}
+        >
+          <p>you can submit your votes now</p>
+          <button
+            style={{
+              background: "#38b000",
+              border: "none",
+              color: "white",
+              padding: "5px 20px",
+              lineBreak: "auto",
+              borderRadius: "5px",
+              marginBottom: "10px",
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      )}
     </div>
   );
 };
