0ea85c2e82854c74b2c34f41a58feeeab3e49dd5
julianapeace <chancecordelia@gmail.com>
2023-10-07 16:00:22 +0700

    reverted to genuine frontend

--- src/compnents/vote.js
+++ src/compnents/vote.js
@@ -3,7 +3,6 @@ import { Link, Paragraph } from "theme-ui";
 import Modal from "./modal";
 import useSound from "use-sound";
 import { encodeSignal } from "../lib";
-import { ProjectsRepository } from "../repositories/projects_repository";
 import { SemaphoreEthers } from "@semaphore-protocol/data";
 import { Group } from "@semaphore-protocol/group";
 import { Identity } from "@semaphore-protocol/identity";
@@ -11,64 +10,6 @@ import { generateProof } from "@semaphore-protocol/proof";
 
 const maxTotalVotes = 60;
 const maxVotesPerCandidate = 20;
-
-// const candidates = [
-//   {
-//     project_id: 1,
-//     title: "Create a web3 Gofundme",
-//     description: " The Problem\n" +
-//       "\n" +
-//       "<b>Currently pop up cities like</b> MuChiangMai (https://www.craft.me/s/mNpnCsIkvRUMDD) and Zuzalu are all looking for a way to track people who contribute to these pop up cities but due to the nature of contribution it's hard to quantify and track.\n" +
-//       "\n" +
-//       "Using an example to explain this , Lets say I am in a pop up city and I decide to host Thai Language Classes ,( basically to help people interact with locals properly )  . I would need help with mic set up, looking for a classroom etc . All those people that help me make the workshop possible can basically interact on the platform and hence make it possible for me and the pop up city organizers to track their contributions.\n" +
-//       "\n" +
-//       "\n" +
-//       " Objective\n" +
-//       "\n" +
-//       "For the basic MVP , these things should work ....\n" +
-//       "\n" +
-//       " 1.  A platform to easily create proposals and invite contributors to it that will help push contributions updates to the Proposals\n" +
-//       "2. Creating a     user profile ( basic email  , name , authentication  )\n" +
-//       "3. A issues panel ,where anyone can raise an issue related to a proposal ( Similar to Github issue in repositories)\n" +
-//       "4. A Pull request system where anyone can raise potential solution and submit that solution for review .\n" +
-//       "\n" +
-//       " \n" +
-//       " Potential Approach\n" +
-//       "\n" +
-//       " This includes quick MVP and also how to make a scalable self hosted solution too .    \n" +
-//       "Bascially a markdown is like a notion page ... It's easy to write up and looks good at the same time .\n" +
-//       " 1. Using Github Repo's , github pages ,Husky or CI/CD and markdown files\n" +
-//       "       a. Basically this solution requires less coding and its faster to implement , if you wanna test out this system .\n" +
-//       "       b. You can create a Repo , in which the title of the repo will be in a predictable format like ``<name-of-popup-city>-<proposal-name>`` this way its easier to search on github for public good related to a specific pop up city\n" +
-//       "       c. In the Repo , bascially you use Markdown to document everyone contributions . so the folder  can be like this\n" +
-//       "        ---viaprize:\n" +
-//       "                          /main.md  -> this will contain sub sections with the details  like Title, Problem statement, Proof that it's being worked on ( this could be images / videos etc)\n" +
-//       "                          /contribution\n" +
-//       "                                /<contribution-title>-<contributor-username>-<date-of-contribution>.md -> this will contain a  mark down with the following sections  Title of contribution , Why you  contributed , Proof of contribution , and Additional Information  , Link to other contributor username .\n" +
-//       "       d.  Now using github books and some documentation tools , it can convert this contribution tracking history markdown files into a website with good UI/UX and SEO .\n" +
-//       "       e. Using Husky or CI/CD , it will make sure to deploy the documentation website when ever there is a push to the main repo\n" +
-//       "\n" +
-//       "2. Forking GitLab and Stream lining this process using this .\n" +
-//       "            a. Bascially fork Gitlab frontend ,",
-//     votes: 0,
-//     image: 'https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png'
-//   },
-//   {
-//     project_id: 2,
-//     title: "Create a blog for Zuzalu",
-//     description: "",
-//     image: "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png",
-//     votes: 0,
-//   },
-//   {
-//     project_id: 3,
-//     title: "Title 3",
-//     description: "",
-//     image: "https://storage.googleapis.com/proudcity/mebanenc/uploads/2021/03/placeholder-image.png",
-//     votes: 0,
-//   },
-//   { title: "string", description: "string #but this will be hmtl string", image: "string #url" }
-// ];
 
 const candidates = [
   {
@@ -155,7 +96,6 @@ const VotingComponent = () => {
   const group_id = process.env.REACT_APP_GROUP_ID;
   const params = new URLSearchParams(window.location.search);
   const secret = params.get("s");
- 
 
   useEffect(() => {
     const load = async () => {
@@ -222,26 +162,45 @@ const VotingComponent = () => {
   const onSubmit = async () => {
     const choices = {};
     stateCandidates.forEach((candidate, index) => {
-      if (candidate.votes > 0) choices[index] = candidate.votes;
+      if (candidate.votes > 0)
+        choices[index] = candidate.votes;
     });
+    console.log(choices);
     let encodedSignal = encodeSignal(choices);
+    console.log("encodedSignal", encodedSignal);
+    const group = new Group(group_id, 16, commitments);
+    const identity = new Identity(secret);
 
-    await fetch("https://aperturs-t3.vercel.app/api/test", {
+    const fullProof = await generateProof(
+      identity,
+      group,
+      group.root,
+      encodedSignal,
+      {
+        wasmFilePath: "./semaphore.wasm",
+        zkeyFilePath: "./semaphore.zkey"
+      }
+    )
+    console.log("fullProof", fullProof);
+
+    const data = {
+      group_id: group_id.toString(),
+      merkle_tree_root: fullProof.merkleTreeRoot,
+      signal: fullProof.signal,
+      identity_nullifier: fullProof.nullifierHash,
+      externalNullifier: fullProof.externalNullifier,
+      proof: JSON.stringify(fullProof.proof),
+    }
+    console.log("data", data);
+
+    await fetch("https://zkvoting-relayer.vercel.app/api/relayer", {
       method: "POST",
-      headers:{
-        'Access-Control-Allow-Origin':"*"
+      headers: {
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        secret,
-        encodedSignal,
-         
-      }),
-
+      body: JSON.stringify(data),
     });
-    setIsDisabled(true);
-    localStorage.setItem("submit", true);
-  };
-  if (!secret) { return null };
+  }
   return (
     <>
       <div
@@ -385,42 +344,42 @@ const VotingComponent = () => {
             );
           })}
         </div>
-          {
-            !isDisabled ? (
-        ((maxTotalVotes === totalVotes) ) ? 
-           (
-          <div
-            style={{
-              background: "black",
-              border: "none",
-              color: "white",
-              padding: "5px 20px",
-              textAlign: "center",
-              borderRadius: "5px",
-              margin: "10px",
-            }}
-          >
-            <>
-              <p>you can submit your votes now</p>
-              <button
-                onClick={() => onSubmit()}
-                style={{
-                  background: "#38b000",
-                  border: "none",
-                  color: "white",
-                  padding: "5px 20px",
-                  lineBreak: "auto",
-                  borderRadius: "5px",
-                  marginBottom: "10px",
-                }}
-              >
-                Submit
-              </button>
-            </>
-          </div>
-        ): null): (<div>
-          Thanks for voting!
-        </div>)}
+        {
+          !isDisabled ? (
+            ((maxTotalVotes === totalVotes)) ?
+              (
+                <div
+                  style={{
+                    background: "black",
+                    border: "none",
+                    color: "white",
+                    padding: "5px 20px",
+                    textAlign: "center",
+                    borderRadius: "5px",
+                    margin: "10px",
+                  }}
+                >
+                  <>
+                    <p>you can submit your votes now</p>
+                    <button
+                      onClick={() => onSubmit()}
+                      style={{
+                        background: "#38b000",
+                        border: "none",
+                        color: "white",
+                        padding: "5px 20px",
+                        lineBreak: "auto",
+                        borderRadius: "5px",
+                        marginBottom: "10px",
+                      }}
+                    >
+                      Submit
+                    </button>
+                  </>
+                </div>
+              ) : null) : (<div>
+                Thanks for voting!
+              </div>)}
       </div>
       {showProjectDetails ? (
         <Modal
